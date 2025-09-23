b810ce2980b90d2aafa7e637e43db14902c0c0e2
Wolfgang Hommel <wolfgang.hommel@unibw.de>
2025-09-23 07:19:29 +0200

    Use relative path in firmware upload script

--- run-relay.sh
+++ run-relay.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-# export LORADEV=/dev/cu.usbserial-0001
-# export LORADEV=/dev/cu.usbserial-7
-platformio run --target upload --upload-port $LORADEV && ~/repos/github/roloran/ROLORAN-Terminal/roloran-terminal.py
+platformio run --target upload --upload-port $LORADEV && ../ROLORAN-Terminal/roloran-terminal.py
